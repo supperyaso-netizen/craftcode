7324cfc6d29974352c0049b97d41b993c8a07d79
Yaso <supperyaso@gmail.com>
2026-07-09 10:16:39 +0530

    Fix edit project page

--- app/admin/projects/[id]/page.tsx
+++ app/admin/projects/[id]/page.tsx
@@ -1,13 +1,7 @@
-// app/admin/projects/[id]/page.tsx
 "use client";
 
-import { useParams } from "next/navigation";
 import NewProjectPage from "../new/page";
 
 export default function EditProjectPage() {
-  const params = useParams();
-  const id = params?.id as string;
-  
-  // Pass the project ID to the NewProjectPage component
-  return <NewProjectPage projectId={id} />;
+  return <NewProjectPage />;
 }
